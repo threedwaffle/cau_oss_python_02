e3b6a98a027b157ced8ff3ea559359785ef790d8
threedwaffle <163142150+threedwaffle@users.noreply.github.com>
2024-03-15 18:03:12 +0900

    Create volume_calc.py
    1주차 박스의 부피 계산

--- prac1.py
+++
@@ -1,7 +0,0 @@
-hw_score  = int(input("Homework:"))
-att_score = int(input("Attendance:"))
-mid_score = int(input("Mid-term exam:"))
-fin_score = int(input("Final exam:"))
-
-total_score = hw_score + att_score + mid_score + fin_score
-print("Total score:", total_score)
